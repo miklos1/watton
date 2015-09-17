4dae7ce524f636aa304619e1fc2059e14a7f1741
Miklos Homolya <m.homolya14@imperial.ac.uk>
2015-09-17 10:51:50 +0100

    fix octal numbers in submission script

--- submit.sh
+++ submit.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-nnodes=$(printf "%04d" "$1")
+nnodes="$1"
 nprocs=$((24 * ${nnodes}))
+id=$(printf "%04d" ${nnodes})
 
-sed -e "s/@@nnodes@@/${nnodes}/g" -e "s/@@nprocs@@/${nprocs}/g" submit.pbs.in > submit.pbs
+sed -e "s/@@id@@/${id}/g" -e "s/@@nnodes@@/${nnodes}/g" -e "s/@@nprocs@@/${nprocs}/g" submit.pbs.in > submit.pbs
 qsub submit.pbs
 rm -f submit.pbs
